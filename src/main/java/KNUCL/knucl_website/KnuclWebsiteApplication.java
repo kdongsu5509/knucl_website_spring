16fd25739b25c4bc131a7e61db9fa8139df4c9eb
kdongsu5509 <kdongsu5509@gmail.com>
2024-11-13 00:05:08 +0900

    feat(project) add project folder to github
    add all project files to github to contorl via version contorl system

---
+++ src/main/java/KNUCL/knucl_website/KnuclWebsiteApplication.java
@@ -0,0 +1,13 @@
+package KNUCL.knucl_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnuclWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnuclWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KNUCL/knucl_website/KnuclWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package KNUCL.knucl_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuclWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
